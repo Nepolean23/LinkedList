d5863758f9a75d0f5ec13e55f324642e11c7c10c
Nepolean23 <nlean1996@gmail.com>
2022-12-08 18:59:06 +0530

    LinkedList Appending Of Numbers 56-30-70

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -3,7 +3,10 @@
 //UC_1_CreatingLinkedList title = new UC_1_CreatingLinkedList();
 //title.CreatingLinkedList();
 
-UC_2_LinkedList_AddingOFNumber_56_30_70 list = new UC_2_LinkedList_AddingOFNumber_56_30_70();
-list.AddingNumbers(56);
-list.AddingNumbers(30);
-list.AddingNumbers(70);
+//UC_2_LinkedList_AddingOFNumber_56_30_70 list = new UC_2_LinkedList_AddingOFNumber_56_30_70();
+//list.AddingNumbers(56);
+//list.AddingNumbers(30);
+//list.AddingNumbers(70);
+
+UC_3_LinkedList_AppendingOfNumbers_56_30_70 Num = new UC_3_LinkedList_AppendingOfNumbers_56_30_70();
+Num.Display();
---
+++ LinkedList/LinkedList/UC_3_LinkedList_AppendingOfNumbers_56_30_70.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class UC_3_LinkedList_AppendingOfNumbers_56_30_70
+    {
+        internal Node head;
+
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + "");
+                temp = temp.next;
+
+            }
+        }
+}
