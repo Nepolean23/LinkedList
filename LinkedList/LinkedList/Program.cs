e5b0ac362533c1f6e3b7efc025c95d3897841c7f
Nepolean23 <nlean1996@gmail.com>
2022-12-08 18:50:23 +0530

    LinkedList Adding Of Number 56-30-70

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -1,4 +1,9 @@
 ﻿using LinkedList;
 
-UC_1_CreatingLinkedList title = new UC_1_CreatingLinkedList();
-title.CreatingLinkedList();
+//UC_1_CreatingLinkedList title = new UC_1_CreatingLinkedList();
+//title.CreatingLinkedList();
+
+UC_2_LinkedList_AddingOFNumber_56_30_70 list = new UC_2_LinkedList_AddingOFNumber_56_30_70();
+list.AddingNumbers(56);
+list.AddingNumbers(30);
+list.AddingNumbers(70);
---
+++ LinkedList/LinkedList/UC_2_LinkedList_AddingOFNumber_56_30_70.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class UC_2_LinkedList_AddingOFNumber_56_30_70
+    {
+        public Node head;
+        public void AddingNumbers(int data)
+        {
+            Node Node = new Node(data);
+            if (this.head == null)
+                this.head = Node;
+            else
+            {
+                Node temp = this.head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = Node;
+            }
+
+            Console.WriteLine("{0} The value added in LinkedList", Node.data);
+
+
+        }
+    }
